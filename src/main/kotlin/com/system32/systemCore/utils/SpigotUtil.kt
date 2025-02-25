c41b35618346aac67cf3d6ede2c93838c9150464
System32_Developer <80436979+system32developer@users.noreply.github.com>
2025-02-25 11:20:00 -0500

    refactor SpigotUtil version check method for improved clarity and functionality

--- src/main/kotlin/com/system32/systemCore/utils/SpigotUtil.kt
+++ src/main/kotlin/com/system32/systemCore/utils/SpigotUtil.kt
@@ -10,17 +10,42 @@ import java.util.function.Consumer
 
 class SpigotUtil (private val plugin: JavaPlugin, private val resourceId: Int) {
 
-    fun version(consumer: Consumer<String>) {
+    /**
+     * Checks if there is a new version of the plugin available on SpigotMC.
+     *
+     * @param onLatest Action to execute if the plugin is up to date.
+     * @param onOutdated Action to execute if a new version is available, receives the latest version as a parameter.
+     *
+     * ### Example usage:
+     * ```
+     * val spigotUtil = SpigotUtil(plugin, 12345) // SpigotMC resource ID
+     * spigotUtil.version(
+     *     onLatest = {
+     *         plugin.logger.info("You are using the latest version.")
+     *     },
+     *     onOutdated = { latestVersion ->
+     *         plugin.logger.info("A new version is available: $latestVersion. Please update!")
+     *     }
+     * )
+     * ```
+     */
+    fun version(onLatest: () -> Unit, onOutdated: (String) -> Unit) {
         Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable {
             try {
-                URL("https://api.spigotmc.org/legacy/update.php?resource=$resourceId/~").openStream()
-                    .use { inputStream ->
-                        Scanner(inputStream).use { scanner ->
-                            if (scanner.hasNext()) {
-                                consumer.accept(scanner.next())
+                URL("https://api.spigotmc.org/legacy/update.php?resource=$resourceId/~").openStream().use { inputStream ->
+                    Scanner(inputStream).use { scanner ->
+                        if (scanner.hasNext()) {
+                            val latestVersion = scanner.next()
+                            val currentVersion = plugin.pluginMeta.version
+
+                            if (currentVersion == latestVersion) {
+                                onLatest()
+                            } else {
+                                onOutdated(latestVersion)
                             }
                         }
                     }
+                }
             } catch (e: IOException) {
                 plugin.logger.info("Unable to check for updates: ${e.message}")
             }
