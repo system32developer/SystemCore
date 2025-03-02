47807c6f0c48844dc62f3a203557c099575ee130
System32_Developer <80436979+system32developer@users.noreply.github.com>
2025-03-02 18:37:47 -0500

    add base64 serialization methods for ItemStack in SpigotUtil

--- src/main/kotlin/com/system32/systemCore/utils/SpigotUtil.kt
+++ src/main/kotlin/com/system32/systemCore/utils/SpigotUtil.kt
@@ -2,13 +2,49 @@ package com.system32.systemCore.utils
 
 import org.bstats.bukkit.Metrics
 import org.bukkit.Bukkit
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.util.io.BukkitObjectInputStream
+import org.bukkit.util.io.BukkitObjectOutputStream
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.net.URL
-import java.util.Scanner
-import java.util.function.Consumer
+import java.util.*
+
 
 class SpigotUtil (private val plugin: JavaPlugin, private val resourceId: Int) {
+
+    companion object{
+        fun fromBase64ItemStack(base64: String): ItemStack? {
+            try {
+                val inputStream = ByteArrayInputStream(Base64Coder.decodeLines(base64))
+                val bukkitInputStream = BukkitObjectInputStream(inputStream)
+                val item = bukkitInputStream.readObject() as ItemStack?
+                bukkitInputStream.close()
+                return item
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: ClassNotFoundException) {
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        fun toBase64(item: ItemStack?): String? {
+            try {
+                val outputStream = ByteArrayOutputStream()
+                val bukkitOutputStream = BukkitObjectOutputStream(outputStream)
+                bukkitOutputStream.writeObject(item)
+                bukkitOutputStream.close()
+                return Base64Coder.encodeLines(outputStream.toByteArray())
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return null
+        }
+    }
 
     /**
      * Checks if there is a new version of the plugin available on SpigotMC.
