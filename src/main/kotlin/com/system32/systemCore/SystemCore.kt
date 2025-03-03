139fddb4c8cfa9fd05c9842a2c01e52e3e68fb29
System32_Developer <80436979+system32developer@users.noreply.github.com>
2025-03-03 12:09:06 -0500

    add register method to SystemCore for event listener registration

--- src/main/kotlin/com/system32/systemCore/SystemCore.kt
+++ src/main/kotlin/com/system32/systemCore/SystemCore.kt
@@ -1,6 +1,8 @@
 package com.system32.systemCore
 
 import com.system32.systemCore.utils.DiscordUtil
+import org.bukkit.event.Event
+import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -16,12 +18,17 @@ class SystemCore {
         fun getInstance(): Plugin {
             return plugin
         }
+
         fun placeHolderAPIHook(): Boolean {
             return placeholderAPI
         }
 
         fun placeHolderAPIHook(boolean: Boolean) {
             placeholderAPI = boolean
+        }
+
+        fun register(event: Listener){
+            plugin.server.pluginManager.registerEvents(event, plugin)
         }
     }
 }
