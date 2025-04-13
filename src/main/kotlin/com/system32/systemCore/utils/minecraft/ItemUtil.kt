4560cba42392c4f1c67a615c452e4a77e0387d9c
System32_Developer <80436979+system32developer@users.noreply.github.com>
2025-04-13 10:59:47 -0500

    Refactor ItemUtil to simplify Base64 serialization and deserialization of ItemStack

--- src/main/kotlin/com/system32/systemCore/utils/minecraft/ItemUtil.kt
+++ src/main/kotlin/com/system32/systemCore/utils/minecraft/ItemUtil.kt
@@ -1,41 +1,26 @@
 package com.system32.systemCore.utils.minecraft
 
 import org.bukkit.inventory.ItemStack
-import org.bukkit.util.io.BukkitObjectInputStream
-import org.bukkit.util.io.BukkitObjectOutputStream
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.util.*
 
 class ItemUtil {
-    companion object{
+    companion object {
         fun fromBase64(base64: String): ItemStack? {
-            try {
-                val inputStream = ByteArrayInputStream(Base64Coder.decodeLines(base64))
-                val bukkitInputStream = BukkitObjectInputStream(inputStream)
-                val item = bukkitInputStream.readObject() as ItemStack?
-                bukkitInputStream.close()
-                return item
-            } catch (e: IOException) {
-                e.printStackTrace()
-            } catch (e: ClassNotFoundException) {
-                e.printStackTrace()
-            }
-            return null
+            val data = Base64.getDecoder().decode(base64)
+            return ItemStack.deserializeBytes(data);
         }
 
         fun toBase64(item: ItemStack?): String? {
-            try {
-                val outputStream = ByteArrayOutputStream()
-                val bukkitOutputStream = BukkitObjectOutputStream(outputStream)
-                bukkitOutputStream.writeObject(item)
-                bukkitOutputStream.close()
-                return Base64Coder.encodeLines(outputStream.toByteArray())
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            return null
+            return Base64.getEncoder().encodeToString(item?.serializeAsBytes())
+        }
+
+        fun fromBase64List(base64: List<String>): List<ItemStack?> {
+            return base64.map { fromBase64(it) }
+        }
+
+        fun toBase64List(items: List<ItemStack?>): List<String?> {
+            return items.map { toBase64(it) }
         }
     }
 }
