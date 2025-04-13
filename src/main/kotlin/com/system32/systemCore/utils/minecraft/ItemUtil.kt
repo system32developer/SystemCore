7c497b46b850a1152f3ba033e3f56725b0bc0b2d
System32_Developer <80436979+system32developer@users.noreply.github.com>
2025-04-13 11:00:25 -0500

    Refactor ItemUtil to enforce non-nullable types for Base64 serialization and deserialization of ItemStack

--- src/main/kotlin/com/system32/systemCore/utils/minecraft/ItemUtil.kt
+++ src/main/kotlin/com/system32/systemCore/utils/minecraft/ItemUtil.kt
@@ -6,20 +6,20 @@ import java.util.*
 
 class ItemUtil {
     companion object {
-        fun fromBase64(base64: String): ItemStack? {
+        fun fromBase64(base64: String): ItemStack {
             val data = Base64.getDecoder().decode(base64)
             return ItemStack.deserializeBytes(data);
         }
 
-        fun toBase64(item: ItemStack?): String? {
-            return Base64.getEncoder().encodeToString(item?.serializeAsBytes())
+        fun toBase64(item: ItemStack): String {
+            return Base64.getEncoder().encodeToString(item.serializeAsBytes())
         }
 
-        fun fromBase64List(base64: List<String>): List<ItemStack?> {
+        fun fromBase64List(base64: List<String>): List<ItemStack> {
             return base64.map { fromBase64(it) }
         }
 
-        fun toBase64List(items: List<ItemStack?>): List<String?> {
+        fun toBase64List(items: List<ItemStack>): List<String> {
             return items.map { toBase64(it) }
         }
     }
