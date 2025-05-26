14f9df82a4b9df238377af9292bc9d043a55a9b0
System32_Developer <80436979+system32developer@users.noreply.github.com>
2025-05-26 12:21:27 -0500

    Refactor ServerUtil and SpigotUtil to use object declaration for cleaner code structure

--- src/main/kotlin/com/system32/systemCore/utils/minecraft/ServerUtil.kt
+++ src/main/kotlin/com/system32/systemCore/utils/minecraft/ServerUtil.kt
@@ -7,72 +7,70 @@ import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitTask
 import java.util.UUID
 
-class ServerUtil {
-    companion object{
-        fun player(uuid: UUID): Player? {
-            return Bukkit.getPlayer(uuid)
-        }
+object ServerUtil {
+    fun player(uuid: UUID): Player? {
+        return Bukkit.getPlayer(uuid)
+    }
 
-        fun player(name: String) : Player?{
-            return Bukkit.getPlayer(name)
-        }
+    fun player(name: String) : Player?{
+        return Bukkit.getPlayer(name)
+    }
 
-        fun offlinePlayer(name: String) : Player?{
-            return Bukkit.getOfflinePlayer(name).player
-        }
+    fun offlinePlayer(name: String) : Player?{
+        return Bukkit.getOfflinePlayer(name).player
+    }
 
-        fun offlinePlayer(uuid: UUID) : Player?{
-            return Bukkit.getOfflinePlayer(uuid).player
-        }
+    fun offlinePlayer(uuid: UUID) : Player?{
+        return Bukkit.getOfflinePlayer(uuid).player
+    }
 
-        fun players() : List<Player>{
-            return Bukkit.getOnlinePlayers().toList()
-        }
+    fun players() : List<Player>{
+        return Bukkit.getOnlinePlayers().toList()
+    }
 
-        fun broadcast(message: String) {
-            Bukkit.broadcast(TextUtil.Companion.color(message))
-        }
+    fun broadcast(message: String) {
+        Bukkit.broadcast(TextUtil.Companion.color(message))
+    }
 
-        fun task(runnable: Runnable) : BukkitTask {
-            return Bukkit.getScheduler().runTask(SystemCore.plugin, runnable)
-        }
+    fun task(runnable: Runnable) : BukkitTask {
+        return Bukkit.getScheduler().runTask(SystemCore.plugin, runnable)
+    }
 
-        fun taskLater(runnable: Runnable, delay: Int) : BukkitTask {
-            return Bukkit.getScheduler().runTaskLater(SystemCore.plugin, runnable, 20L * delay)
-        }
+    fun taskLater(runnable: Runnable, delay: Int) : BukkitTask {
+        return Bukkit.getScheduler().runTaskLater(SystemCore.plugin, runnable, 20L * delay)
+    }
 
-        fun taskTimer(runnable: Runnable, delay: Int, period: Int) : BukkitTask {
-            return Bukkit.getScheduler().runTaskTimer(SystemCore.plugin, runnable, 20L * delay, 20L * period)
-        }
+    fun taskTimer(runnable: Runnable, delay: Int, period: Int) : BukkitTask {
+        return Bukkit.getScheduler().runTaskTimer(SystemCore.plugin, runnable, 20L * delay, 20L * period)
+    }
 
-        fun taskAsync(runnable: Runnable) : BukkitTask {
-            return Bukkit.getScheduler().runTaskAsynchronously(SystemCore.plugin, runnable)
-        }
+    fun taskAsync(runnable: Runnable) : BukkitTask {
+        return Bukkit.getScheduler().runTaskAsynchronously(SystemCore.plugin, runnable)
+    }
 
-        fun taskLaterAsync(runnable: Runnable, delay: Int) : BukkitTask {
-            return Bukkit.getScheduler().runTaskLaterAsynchronously(SystemCore.plugin, runnable, 20L * delay)
-        }
+    fun taskLaterAsync(runnable: Runnable, delay: Int) : BukkitTask {
+        return Bukkit.getScheduler().runTaskLaterAsynchronously(SystemCore.plugin, runnable, 20L * delay)
+    }
 
-        fun taskTimerAsync(runnable: Runnable, delay: Int, period: Int) : BukkitTask {
-            return Bukkit.getScheduler().runTaskTimerAsynchronously(SystemCore.plugin, runnable, 20L * delay, 20L * period)
-        }
+    fun taskTimerAsync(runnable: Runnable, delay: Int, period: Int) : BukkitTask {
+        return Bukkit.getScheduler().runTaskTimerAsynchronously(SystemCore.plugin, runnable, 20L * delay, 20L * period)
+    }
 
-        //With Long
+    //With Long
 
-        fun taskLater(runnable: Runnable, delay: Long) : BukkitTask {
-            return Bukkit.getScheduler().runTaskLater(SystemCore.plugin, runnable, delay)
-        }
+    fun taskLater(runnable: Runnable, delay: Long) : BukkitTask {
+        return Bukkit.getScheduler().runTaskLater(SystemCore.plugin, runnable, delay)
+    }
 
-        fun taskTimer(runnable: Runnable, delay: Long, period: Long) : BukkitTask {
-            return Bukkit.getScheduler().runTaskTimer(SystemCore.plugin, runnable, delay, period)
-        }
+    fun taskTimer(runnable: Runnable, delay: Long, period: Long) : BukkitTask {
+        return Bukkit.getScheduler().runTaskTimer(SystemCore.plugin, runnable, delay, period)
+    }
 
-        fun taskLaterAsync(runnable: Runnable, delay: Long) : BukkitTask {
-            return Bukkit.getScheduler().runTaskLaterAsynchronously(SystemCore.plugin, runnable, delay)
-        }
+    fun taskLaterAsync(runnable: Runnable, delay: Long) : BukkitTask {
+        return Bukkit.getScheduler().runTaskLaterAsynchronously(SystemCore.plugin, runnable, delay)
+    }
 
-        fun taskTimerAsync(runnable: Runnable, delay: Long, period: Long) : BukkitTask {
-            return Bukkit.getScheduler().runTaskTimerAsynchronously(SystemCore.plugin, runnable, delay, period)
-        }
+    fun taskTimerAsync(runnable: Runnable, delay: Long, period: Long) : BukkitTask {
+        return Bukkit.getScheduler().runTaskTimerAsynchronously(SystemCore.plugin, runnable, delay, period)
     }
 }
--- src/main/kotlin/com/system32/systemCore/utils/minecraft/SpigotUtil.kt
+++ src/main/kotlin/com/system32/systemCore/utils/minecraft/SpigotUtil.kt
@@ -12,74 +12,69 @@ import java.io.IOException
 import java.net.URL
 import java.util.Scanner
 
-class SpigotUtil {
+object SpigotUtil{
+    fun key(key: String): NamespacedKey {
+        return NamespacedKey(SystemCore.plugin, key)
+    }
 
-    companion object{
+    fun hearts(amount: Double): Double{
+        return amount * 2
+    }
 
-        fun key(key: String): NamespacedKey {
-            return NamespacedKey(SystemCore.plugin, key)
-        }
+    fun center(block: Block): Location {
+        return block.location.add(0.5, 0.5, 0.5)
+    }
 
-        fun hearts(amount: Double): Double{
-            return amount * 2
-        }
+    fun center(block: Block, block2: Block): Location {
+        return block.location.add((block2.location.x - block.location.x) / 2, (block2.location.y - block.location.y) / 2, (block2.location.z - block.location.z) / 2)
+    }
+    /**
+     * Checks if there is a new version of the plugin available on SpigotMC.
+     *
+     * @param onLatest (Optional) Action to execute if the plugin is up to date. Default is an empty function.
+     * @param onOutdated Action to execute if a new version is available. It provides:
+     *  - `latestVersion`: The latest version available on SpigotMC.
+     *  - `downloadLink`: The direct link to the plugin's resource page on SpigotMC.
+     *
+     * ### Example usage:
+     * ```
+     * val spigotUtil = SpigotUtil(plugin, 12345) // SpigotMC resource ID
+     * spigotUtil.version(
+     *    onLatest = {
+     *      plugin.logger.info("You are running the latest version.")
+     *    },
+     *     onOutdated = { latestVersion, downloadLink ->
+     *      plugin.logger.info("A new version ($latestVersion) is available! Download it here: $downloadLink")
+     *     }
+     * )
+     * ```
+     */
+    fun version(resourceId: Int, onLatest: () -> Unit = {}, onOutdated: (latestVersion: String, downloadLink: String) -> Unit) {
+        val plugin: Plugin = SystemCore.plugin
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable {
+            try {
+                URL("https://api.spigotmc.org/legacy/update.php?resource=$resourceId/~").openStream().use { inputStream ->
+                    Scanner(inputStream).use { scanner ->
+                        if (scanner.hasNext()) {
+                            val latestVersion = scanner.next()
+                            val currentVersion = plugin.pluginMeta.version
+                            val downloadLink = "https://www.spigotmc.org/resources/$resourceId"
 
-        fun center(block: Block): Location {
-            return block.location.add(0.5, 0.5, 0.5)
-        }
-
-        fun center(block: Block, block2: Block): Location {
-            return block.location.add((block2.location.x - block.location.x) / 2, (block2.location.y - block.location.y) / 2, (block2.location.z - block.location.z) / 2)
-        }
-        /**
-         * Checks if there is a new version of the plugin available on SpigotMC.
-         *
-         * @param onLatest (Optional) Action to execute if the plugin is up to date. Default is an empty function.
-         * @param onOutdated Action to execute if a new version is available. It provides:
-         *  - `latestVersion`: The latest version available on SpigotMC.
-         *  - `downloadLink`: The direct link to the plugin's resource page on SpigotMC.
-         *
-         * ### Example usage:
-         * ```
-         * val spigotUtil = SpigotUtil(plugin, 12345) // SpigotMC resource ID
-         * spigotUtil.version(
-         *    onLatest = {
-         *      plugin.logger.info("You are running the latest version.")
-         *    },
-         *     onOutdated = { latestVersion, downloadLink ->
-         *      plugin.logger.info("A new version ($latestVersion) is available! Download it here: $downloadLink")
-         *     }
-         * )
-         * ```
-         */
-        fun version(resourceId: Int, onLatest: () -> Unit = {}, onOutdated: (latestVersion: String, downloadLink: String) -> Unit) {
-            val plugin: Plugin = SystemCore.plugin
-            Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable {
-                try {
-                    URL("https://api.spigotmc.org/legacy/update.php?resource=$resourceId/~").openStream().use { inputStream ->
-                        Scanner(inputStream).use { scanner ->
-                            if (scanner.hasNext()) {
-                                val latestVersion = scanner.next()
-                                val currentVersion = plugin.pluginMeta.version
-                                val downloadLink = "https://www.spigotmc.org/resources/$resourceId"
-
-                                if (currentVersion == latestVersion) {
-                                    onLatest()
-                                } else {
-                                    onOutdated(latestVersion, downloadLink)
-                                }
+                            if (currentVersion == latestVersion) {
+                                onLatest()
+                            } else {
+                                onOutdated(latestVersion, downloadLink)
                             }
                         }
                     }
-                } catch (e: IOException) {
-                    plugin.logger.info("Unable to check for updates: ${e.message}")
                 }
-            })
-        }
-
-        fun metrics(id: Int, plugin: JavaPlugin) : Metrics {
-            return Metrics(plugin, id)
-        }
+            } catch (e: IOException) {
+                plugin.logger.info("Unable to check for updates: ${e.message}")
+            }
+        })
     }
 
+    fun metrics(id: Int, plugin: JavaPlugin) : Metrics {
+        return Metrics(plugin, id)
+    }
 }
