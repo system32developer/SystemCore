c95eec77189300dcb0d23d23b2aa6751b78bd58b
system32developer <internaldevz@gmail.com>
2025-08-24 21:30:49 -0500

    Add Service annotation and ServiceProcessor for service management

---
+++ src/main/kotlin/com/system32/systemCore/managers/service/ServiceManager.kt
@@ -0,0 +1,7 @@
+package com.system32.systemCore.managers.service
+
+object ServiceManager {
+    init {
+
+    }
+}
--- src/main/kotlin/com/system32/systemCore/managers/service/processor/ServiceProcessor.kt
+++ src/main/kotlin/com/system32/systemCore/managers/service/processor/ServiceProcessor.kt
@@ -1,6 +1,7 @@
 package com.system32.systemCore.managers.service.processor
 import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.*
+import com.system32.systemCore.managers.service.PluginService
 import com.system32.systemCore.managers.service.annotation.Service
 import java.io.OutputStreamWriter
 
@@ -13,23 +14,29 @@ class ServiceProcessor(
         val symbols = resolver.getSymbolsWithAnnotation(Service::class.qualifiedName!!)
             .filterIsInstance<KSClassDeclaration>()
 
+        println(symbols.count())
+
+        println(symbols.map { it.qualifiedName!!.asString() })
+
         if (!symbols.iterator().hasNext()) return emptyList()
 
-        val serviceInterface = "com.system32.systemCore.managers.service.PluginService"
-
+        val serviceInterface = PluginService::class.qualifiedName!!
         val file = codeGenerator.createNewFile(
             Dependencies(false),
-            "",
-            "META-INF/services/$serviceInterface"
+            "META-INF/services/",
+            serviceInterface,
+            ""
         )
 
         OutputStreamWriter(file, Charsets.UTF_8).use { writer ->
             for (symbol in symbols) {
                 val className = symbol.qualifiedName?.asString()
-                if (className != null && symbol.superTypes.any { it.resolve().declaration.qualifiedName?.asString() == serviceInterface }) {
+                if (className != null &&
+                    symbol.superTypes.any { it.resolve().declaration.qualifiedName?.asString() == serviceInterface }
+                ) {
                     writer.write("$className\n")
                 } else {
-                    logger.warn("La clase $className tiene @Service pero no implementa $serviceInterface")
+                    logger.warn("The class $className is annotated with @Service but does not implement PluginService interface.")
                 }
             }
         }
