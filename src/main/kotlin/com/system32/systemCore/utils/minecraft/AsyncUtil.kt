37c4b6bb8fc4716207df11b39d4eac0d89f6bcd5
system32developer <internaldevz@gmail.com>
2025-08-29 17:06:53 -0500

    Refactor AsyncTask to use CompletableFuture for improved asynchronous handling

--- src/main/kotlin/com/system32/systemCore/utils/minecraft/AsyncUtil.kt
+++ src/main/kotlin/com/system32/systemCore/utils/minecraft/AsyncUtil.kt
@@ -1,34 +1,31 @@
 package com.system32.systemCore.utils.minecraft
 
-import com.system32.systemCore.utils.minecraft.ServerUtil.task
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
+import java.util.concurrent.CompletableFuture
 
-object AsyncManager {
-    val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-}
-
-class AsyncTask<T>(private val block: suspend () -> T) {
-    private var onComplete: ((T) -> Unit)? = null
-
-    infix fun onComplete(block: (T) -> Unit) {
-        onComplete = block
-    }
+class AsyncTask<T>(private val block: () -> T) {
+    private val future = CompletableFuture<T>()
 
     fun launch() {
-        AsyncManager.scope.launch {
-            val result = block()
-            task {
-                onComplete?.invoke(result)
-            }
+        CompletableFuture.supplyAsync {
+            block()
+        }.thenAccept { result ->
+            future.complete(result)
+        }.exceptionally { ex ->
+            future.completeExceptionally(ex)
+            null
         }
+    }
+
+    fun onComplete(action: (T) -> Unit): AsyncTask<T> {
+        future.thenAccept { result ->
+            action(result)
+        }
+        return this
     }
 }
 
-fun <T> async(block: suspend () -> T): AsyncTask<T> {
-    val t = AsyncTask(block)
-    t.launch()
-    return t
+fun <T> async(block: () -> T): AsyncTask<T> {
+    val task = AsyncTask(block)
+    task.launch()
+    return task
 }
