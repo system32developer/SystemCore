6bda17882b5fc241e5249895f9ab1054c6f7c725
system32developer <internaldevz@gmail.com>
2025-09-04 22:13:07 -0500

    Change Event annotation retention policy to RUNTIME and improve logging messages in EventProcessor

--- src/main/kotlin/com/system32/systemCore/managers/processor/annotations/Event.kt
+++ src/main/kotlin/com/system32/systemCore/managers/processor/annotations/Event.kt
@@ -3,7 +3,7 @@ package com.system32.systemCore.managers.processor.annotations
 import org.bukkit.event.EventPriority
 
 @Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.BINARY)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class Event(
     val priority: EventPriority = EventPriority.NORMAL,
     val ignoreCancelled: Boolean = false
