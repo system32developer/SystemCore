6c6d93c46693708b297f260bd6893b1c34b4a5ce
system32developer <internaldevz@gmail.com>
2025-09-26 16:47:28 -0500

    Add @Repeatable annotation to Dependency class for improved flexibility

--- src/main/kotlin/com/system32/systemCore/managers/processor/annotations/Repository.kt
+++ src/main/kotlin/com/system32/systemCore/managers/processor/annotations/Repository.kt
@@ -2,6 +2,7 @@ package com.system32.systemCore.managers.processor.annotations
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
+@Repeatable
 annotation class Dependency(
     val coordinates: String,
     val repository: String = ""
