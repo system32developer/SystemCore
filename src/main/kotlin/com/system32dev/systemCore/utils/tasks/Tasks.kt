0272c691b3ef114e5bedebb3d4ca36fd11f1dcd8
System32_Developer <system32developer@gmail.com>
2026-03-09 11:13:20 -0500

    Add utility functions for task scheduling with delay and timer options

--- src/main/kotlin/com/system32dev/systemCore/utils/tasks/Tasks.kt
+++ src/main/kotlin/com/system32dev/systemCore/utils/tasks/Tasks.kt
@@ -5,6 +5,22 @@ import org.bukkit.Bukkit
 import org.bukkit.scheduler.BukkitTask
 import java.util.concurrent.TimeUnit
 
+val Int.ticks: Long
+    get() = this.toLong()
+
+val Int.seconds: Long
+    get() = this * 20L
+
+val Int.minutes: Long
+    get() = this * 20L * 60
+
+val Int.hours: Long
+    get() = this * 20L * 60 * 60
+
+val Int.days: Long
+    get() = this * 20L * 60 * 60 * 24
+
+
 fun task(delay: Int = 0, unit: TaskUnit = TaskUnit.SECONDS, block: (BukkitTask) -> Unit): BukkitTask {
     var task: BukkitTask? = null
     task = Bukkit.getScheduler().runTaskLater(SystemCore.plugin, Runnable {
@@ -40,5 +56,29 @@ fun taskTimerAsync(period: Int, unit: TaskUnit = TaskUnit.SECONDS, block: (Bukki
     task = Bukkit.getScheduler().runTaskTimerAsynchronously(SystemCore.plugin, Runnable {
         block(task!!)
     }, 0L, unit.toTicks(period))
+    return task
+}
+
+fun task(delayTicks: Long, block: (BukkitTask) -> Unit): BukkitTask {
+    var task: BukkitTask? = null
+    task = Bukkit.getScheduler().runTaskLater(SystemCore.plugin, Runnable { block(task!!) }, delayTicks)
+    return task
+}
+
+fun taskAsync(delayTicks: Long, block: (BukkitTask) -> Unit): BukkitTask {
+    var task: BukkitTask? = null
+    task = Bukkit.getScheduler().runTaskLaterAsynchronously(SystemCore.plugin, Runnable { block(task!!) }, delayTicks)
+    return task
+}
+
+fun taskTimer(periodTicks: Long, block: (BukkitTask) -> Unit): BukkitTask {
+    var task: BukkitTask? = null
+    task = Bukkit.getScheduler().runTaskTimer(SystemCore.plugin, Runnable { block(task!!) }, 0L, periodTicks)
+    return task
+}
+
+fun taskTimerAsync(periodTicks: Long, block: (BukkitTask) -> Unit): BukkitTask {
+    var task: BukkitTask? = null
+    task = Bukkit.getScheduler().runTaskTimerAsynchronously(SystemCore.plugin, Runnable { block(task!!) }, 0L, periodTicks)
     return task
 }
